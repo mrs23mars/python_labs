cd17fbcd78b9f79d70eb43ac1c32265c437b003c
mrs23mars <meinesa23@gmail.com>
2026-09-14 10:15:18 +0300

    solved tasks

---
+++ src/lab01/04_minutes_to_hhmm.py
@@ -0,0 +1,4 @@
+minutes = int(input('Минуты: '))
+hh = minutes//60
+mm = minutes%60
+print(f'{hh}:{mm}')
---
+++ src/lab01/05_initials_and_len.py
@@ -0,0 +1,3 @@
+FIO = str(input('ФИО: '))
+print(f'Инициалы: {FIO[0]}.')
+print(f'Длина (символов): {len(FIO)}')
