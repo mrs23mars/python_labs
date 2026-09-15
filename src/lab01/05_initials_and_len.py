23e18a39a045a9b805a90f22663108a02f01cdec
mrs23mars <meinesa23@gmail.com>
2026-09-15 22:52:16 +0300

    подправила 5 и 1

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,4 +1,4 @@
 FIO = input('ФИО: ')
 a, b, c = FIO.split()
-print(f'a[0], b[0], c[0]')
-print(f'Длина (символов): {len(FIO)}') 
+print(f'Инициалы: {a[0]}{b[0]}{c[0]}')
+print(f'Длина (символов): {len(a) + len(b) + len(c) + 2}') 
