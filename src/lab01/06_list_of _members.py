3e96179f9ad9dbb735f38703d10ac5887358e8e2
mrs23mars <meinesa23@gmail.com>
2026-09-16 13:22:21 +0300

    исправление ошибок

--- src/lab01/06_list_of _members.py
+++ src/lab01/06_list_of _members.py
@@ -1,11 +1,14 @@
-n = int(input())
+n = int(input('in_1: '))
 
 count_och = 0
 
+cin = 1
+
 for i in range(n):
-    member = input()
+    cin += 1
+    member = input(f'in_{cin}: ')
     a, b, c, d = member.split()
     if d == 'True':
         count_och += 1
 
-print(count_och, n - count_och)
+print(f'out: {count_och} {n - count_och}')
