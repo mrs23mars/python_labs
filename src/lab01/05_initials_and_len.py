3b09cc2f6bd9d367160f41b60aa9770d0cd8d26f
mrs23mars <meinesa23@gmail.com>
2026-09-16 13:25:59 +0300

    добавила 5 фото

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,4 +1,4 @@
 FIO = input('ФИО: ')
 a, b, c = FIO.split()
-print(f'Инициалы: {a[0]}{b[0]}{c[0]}')
+print(f'Инициалы: {a[0]}{b[0]}{c[0]}.')
 print(f'Длина (символов): {len(a) + len(b) + len(c) + 2}') 
